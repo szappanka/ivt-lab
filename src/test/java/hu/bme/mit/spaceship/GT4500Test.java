bb7def080c3c666f3032adda5f28b1a464519c4b
szappanka <panka.szalka@gmail.com>
2022-04-01 13:18:33 +0200

    B modified

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -29,7 +29,7 @@ public class GT4500Test {
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
-
+    int i = 20;
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
